005f22c13a31672334d3f2737a65b6982ebeb652
jordon <jordon.programming@gmail.com>
2023-06-18 10:35:57 +0900

    Initial commit

---
+++ src/main/java/biz/cashewnut/CashewnutApplication.java
@@ -0,0 +1,13 @@
+package biz.cashewnut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashewnutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashewnutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/biz/cashewnut/CashewnutApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.cashewnut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashewnutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
