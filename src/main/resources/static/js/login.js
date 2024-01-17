19ab36bb4e7234bbbb41f5a6250ec83e7278aa2f
Beomsu Jeong <jordon.programming@gmail.com>
2024-01-17 14:00:05 +0900

    000.000.005v 카카오 로그인 /home 리다이렉션 완료

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -86,37 +86,14 @@ function kakaoLogout() {
 // API KEY : AIzaSyAqQNw8z9o9VSp5IEhKMz2Y1THcqZ-bkhQ
 
 
-function loginWithGoogle() {
-    gapi.load('auth2', function() {
-        gapi.auth2.init();
-        options = new gapi.auth2.SigninOptionsBuilder();
-        options.setPrompt('select_account');
-        // 추가는 Oauth 승인 권한 추가 후 띄어쓰기 기준으로 추가
-        options.setScope('email profile openid https://www.googleapis.com/auth/user.birthday.read');
-        // 인스턴스의 함수 호출 - element에 로그인 기능 추가
-        // GgCustomLogin은 li태그안에 있는 ID, 위에 설정한 options와 아래 성공,실패시 실행하는 함수들
-        gapi.auth2.getAuthInstance().attachClickHandler('GgCustomLogin', options, onSignIn, onSignInFailure);
-    })
+function onSignIn(googleUser) {
+    let profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
 
-function onSignIn(googleUser) {
-    var access_token = googleUser.getAuthResponse().access_token
-    $.ajax({
-        // people api를 이용하여 프로필 및 생년월일에 대한 선택동의후 가져온다.
-        url: 'https://people.googleapis.com/v1/people/me'
-        // key에 자신의 API 키를 넣습니다.
-        , data: {personFields:'birthdays', key:'AIzaSyAqQNw8z9o9VSp5IEhKMz2Y1THcqZ-bkhQ', 'access_token': access_token}
-        , method:'GET'
-    })
-        .done(function(e){
-            //프로필을 가져온다.
-            var profile = googleUser.getBasicProfile();
-            console.log(profile)
-        })
-        .fail(function(e){
-            console.log(e);
-        })
-}
-function onSignInFailure(t){
-    console.log(t);
+function onSignInFailure(error) {
+    // Code to handle sign-in failure
 }
