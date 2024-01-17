9585c845474d7ae0db511ec8c1765ef9e5080944
Beomsu Jeong <jordon.programming@gmail.com>
2024-01-17 13:20:35 +0900

    카카오 로그인 /home 리다이렉션 완료

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -57,9 +57,10 @@ Kakao.API.request({
 
         let callback = () => {
             console.log("============세션 충전 완료============")
+            window.location.href = '/home';
         }
         console.log("============세션 충전 중============")
-        callModule.call("/login/kakaoSession", data, callback);
+        RequestUrl("/login/kakaoSession", "POST", data, callback);
     },
     fail: function (error) {
         alert('카카오 로그인에 실패했습니다. 관리자에게 문의하세요.' + JSON.stringify(error));
